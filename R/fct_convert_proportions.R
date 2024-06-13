4bd2da78fb1ec8447161460510a641fe0ca2637f
ClementC <clement.claustre@gmail.com>
2024-06-13 09:49:07 +0200

    Ajout fichiers déploiement shinyapps + résolution bug convertion quantités

--- R/fct_convert_proportions.R
+++ R/fct_convert_proportions.R
@@ -5,10 +5,10 @@
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
-convert_proportions <- function(tab, base_nb_personnes, new_nb_personnes){
-  if(base_nb_personnes == new_nb_personnes) return(tab)
+convert_proportions <- function(tab, old_nb_personnes, new_nb_personnes){
+  if(old_nb_personnes == new_nb_personnes) return(tab)
 
-  ratio = new_nb_personnes/base_nb_personnes
+  ratio = new_nb_personnes/old_nb_personnes
   tab$quantite <- round(tab$quantite*ratio, 1)
   return(tab)
 }
--- dev/01_start.R
+++ dev/01_start.R
@@ -74,17 +74,17 @@ rstudioapi::navigateToFile("dev/02_dev.R")
 
 #Initialisation de la base de données avec les données historiques
 
-library(RSQLite)
-mydb <- dbConnect(RSQLite::SQLite(), "inst/bdd/bdd_recette.sqlite")
-
-recettes <- read.csv2("C:/Users/PC-Clem/Desktop/appli/appli V2/data/recette.csv")
-ingredients <- read.csv2("C:/Users/PC-Clem/Desktop/appli/appli V2/data/ingredients.csv")
-instructions <- read.csv2("C:/Users/PC-Clem/Desktop/appli/appli V2/data/instructions.csv")
-
-
-dbWriteTable(mydb, "recettes", recettes, overwrite = FALSE)
-dbWriteTable(mydb, "ingredients", ingredients, overwrite = FALSE)
-dbWriteTable(mydb, "instructions", instructions, overwrite = FALSE)
-
-dbListTables(mydb)
-dbDisconnect(mydb)
+# library(RSQLite)
+# mydb <- dbConnect(RSQLite::SQLite(), "inst/bdd/bdd_recette.sqlite")
+#
+# recettes <- read.csv2("C:/Users/PC-Clem/Desktop/appli/appli V2/data/recette.csv")
+# ingredients <- read.csv2("C:/Users/PC-Clem/Desktop/appli/appli V2/data/ingredients.csv")
+# instructions <- read.csv2("C:/Users/PC-Clem/Desktop/appli/appli V2/data/instructions.csv")
+#
+#
+# dbWriteTable(mydb, "recettes", recettes, overwrite = FALSE)
+# dbWriteTable(mydb, "ingredients", ingredients, overwrite = FALSE)
+# dbWriteTable(mydb, "instructions", instructions, overwrite = FALSE)
+#
+# dbListTables(mydb)
+# dbDisconnect(mydb)
